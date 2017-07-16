ca03a9f851299cd031ba791c26042b8b9ada3b70
Resal Ramdahadi <resalramdahadi92@gmail.com>
2017-07-16 22:53:12 +0700

    migrate->user->model factory->seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        //model factory->user
+        factory(\App\User::class,10)->create();
     }
 }
