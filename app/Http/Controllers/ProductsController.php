002d426ef7cf3c8d9db62ee55899945afb9defb8
Resal Ramdahadi <resalramdahadi92@gmail.com>
2017-07-17 23:33:01 +0700

    product->delete->try->catch->response

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -72,6 +72,12 @@ class ProductsController extends Controller
      */
     public function destroy($id)
     {
-        //
+        try {
+            Product::destroy($id);
+
+            return response([],204);
+        } catch (Exception $e) {
+            return response(['Problem deleting the product',500]);
+        }
     }
 }
